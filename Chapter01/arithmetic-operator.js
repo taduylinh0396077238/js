ee1b4cfa62efb8c6ee6dbb550f9b0240cfaab3df
Duy Linh <cudotn111@gmail.com>
2021-11-17 00:48:44 +0700

    Duy Linh

---
+++ Chapter01/arithmetic-operator.js
@@ -0,0 +1,11 @@
+var subtotal = (13 + 1) * 5;
+var shipping = 0.5 * (13 + 1);
+var total = subtotal + shipping;
+var elSub = document.getElementById( elementld: 'subtotal');
+elSub.textContent = subtotal;
+var elShip = document.getElementById( elementld: 'shipping');
+elShipping.textContent = shipping;
+var elTotal = document.getElementById( elementld: 'total');
+eltotal.textContent = total;
+
+
---
+++ Chapter01/numeric-variable.js
@@ -0,0 +1,8 @@
+var prince;
+var quantity;
+var total;
+prince = 5;
+quantity = 14;
+total = prince * quantity;
+var el = document.getElementById( elementld: 'const' );
+el.textContent = '$' + total;
